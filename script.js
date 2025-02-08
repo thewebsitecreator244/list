906298c987ee72e5ee45b3641fc312b1429f8cce
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-02-08 09:48:08 +0100

    feat: added element removal

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const addbutton = document.querySelector(".button");
 const formtask = document.querySelector(".form-task");
 const tasklist = document.querySelector(".task-list")
 
+
 function addNewTask(event){
     event.preventDefault();
     const task = formtask.value;
     const newItem = document.querySelector("#template").cloneNode(true).content;
-    newItem.querySelector(".task__text").value = task
-    tasklist.append(newItem)
-    formtask.value = ""
-    formtask.focus()
+    newItem.querySelector(".task__text").value = task;
+    newItem.querySelector(".task__delete").addEventListener("click",deleteTask);
+    tasklist.append(newItem);
+    formtask.value = "";
+    formtask.focus();
+
+}
+
+function deleteTask(event) {
+    let target = event.target.parentElement
+    target.remove()
+    
+    
 }
 addbutton.addEventListener("click",addNewTask);
