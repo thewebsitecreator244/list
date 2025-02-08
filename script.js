8e9f5bc0fb0e24c8a23cd1a2e588c97e836c2a0f
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-02-08 09:38:12 +0100

    feat: added note adding

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const formtask = document.querySelector(".form-task");
 const tasklist = document.querySelector(".task-list")
 
 function addNewTask(event){
-    event.preventDefault()
+    event.preventDefault();
+    const task = formtask.value;
     const newItem = document.querySelector("#template").cloneNode(true).content;
+    newItem.querySelector(".task__text").value = task
+    tasklist.append(newItem)
+    formtask.value = ""
+    formtask.focus()
 }
-addbutton.addEventListener("click",addNewTask)
+addbutton.addEventListener("click",addNewTask);
